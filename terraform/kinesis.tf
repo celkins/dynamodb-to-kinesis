216169b57bba4d643e7b18e0b131c254f4f4246a
Christopher Elkins <celkins@users.noreply.github.com>
2017-08-03 00:11:34 -0700

    Consolidate Terraform config

--- terraform/kinesis.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_kinesis_stream" "stream" {
-  name        = "dynamodb-to-kinesis"
-  shard_count = 1
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_dynamodb_table" "table" {
+  name           = "dynamodb-to-kinesis"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+}
+
+resource "aws_kinesis_stream" "stream" {
+  name        = "dynamodb-to-kinesis"
+  shard_count = 1
+}
